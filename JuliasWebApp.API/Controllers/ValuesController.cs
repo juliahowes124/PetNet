c03df83da0e2d60cf7e3d21bad9f787b6689b51c
Julia Howes <juliahowes@Julias-MacBook-Air-235.local>
2020-07-16 15:33:00 -0500

    Can GetValues

--- JuliasWebApp.API/Controllers/ValuesController.cs
+++ JuliasWebApp.API/Controllers/ValuesController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JuliasWebApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -10,36 +12,24 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value); //200 Ok response. If there is no matching id for x, returns 204 no response or NULL
         }
     }
 }
---
+++ JuliasWebApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using JuliasWebApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JuliasWebApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {}
+        public DbSet<Value> Values { get; set; }
+    }
+}
