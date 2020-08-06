8a25b07128510ebd32137707ef35b808815ea9b8
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-08-05 22:57:37 -0700

    Extended Animal Model

--- AnimalShelter.API/Migrations/20200806055327_ExtendedAnimalEntity.Designer.cs
+++ AnimalShelter.API/Migrations/20200806055327_ExtendedAnimalEntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AnimalShelter.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AnimalShelter.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200806055327_ExtendedAnimalEntity")]
+    partial class ExtendedAnimalEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AnimalShelter.API/Models/Animal.cs
+++ AnimalShelter.API/Models/Animal.cs
@@ -8,11 +8,19 @@ namespace AnimalShelter.API.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Species { get; set; }
+        public string Breed { get; set; }
         public string Description { get; set; }
+        public int AdoptionFee { get; set; }
+        public string Likes { get; set; }
+        public string Qualities { get; set; }
+        public string GoodWith { get; set; }
         public int Age { get; set; }
         public string Gender { get; set; }
         public DateTime Posted { get; set; }
         public DateTime TimeLeftToAdopt { get; set; }
+        public int Views { get; set; }
+        public int Saves { get; set; }
+        public int Inquiries { get; set; }
         public int UserId { get; set; } 
         public ICollection<Photo> Photos { get; set; }
     }
