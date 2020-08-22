5c58751cbcee96a5e2ad6ac531157aa9857e2c37
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-08-21 20:37:25 -0700

    Pic displayed in nav by sending photos w/ login

--- AnimalShelter.API/Data/AuthRepository.cs
+++ AnimalShelter.API/Data/AuthRepository.cs
@@ -15,7 +15,7 @@ namespace AnimalShelter.API.Data
         public async Task<User> Login(string username, string password)
         {
             //retrieve user from database
-            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+            var user = await _context.Users.Include(u => u.ProfilePicture).FirstOrDefaultAsync(x => x.Username == username);
             
             //see if user exists
             if (user == null)
