b4b5e3dca795a634b73e244e9bd148a059f4bb01
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-09-02 13:47:55 -0700

    added background to home page

--- AnimalShelter-SPA/src/app/home/home.component.ts
+++ AnimalShelter-SPA/src/app/home/home.component.ts
@@ -7,18 +7,9 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  // registerMode = false;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
-
-  // registerToggle() {
-  //   this.registerMode = true;
-  // }
-
-  // cancelRegisterMode(registerMode: boolean) {
-  //   this.registerMode = registerMode;
-  // }
 
 }
